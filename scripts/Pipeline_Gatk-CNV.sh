ef0ba5e59cdb49d88cafb3ed4fbd1bb191103f69
vandeclecio <clecio.de@gmail.com>
2025-03-04 18:07:37 -0300

    updated

--- scripts/Pipeline_Gatk-CNV.sh
+++ scripts/Pipeline_Gatk-CNV.sh
@@ -5,12 +5,13 @@
 
 # PARAMETROS OBRIGATORIOS
 SCRATCH60="/home/scratch60/vlira_20nov2023/"
+SCRATCH90="/home/scratch90/vlira_05ago2024/"
 
 #DATA=$(date "+%F") # EDITE SE QUISER USAR UMA PASTA DE UMA DATA ESPECIFICA 
 DATA="2023-11-07"
-OUTPUT_DIR=$SCRATCH60"/Result_Gatk-CNV."$DATA
+OUTPUT_DIR=$SCRATCH90"/Result_Gatk-CNV."$DATA
 
-INPUT_DIR="/home/scratch60/rtorreglosa_12jan2024/preprocessing_READ_result/"
+INPUT_DIR="/home/SCRATCH90/rtorreglosa_12jan2024/preprocessing_READ_result/"
 BAM_FILES=$(find "$INPUT_DIR" -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam')
 JOBS=5
 mem=280
@@ -23,17 +24,19 @@ CUTOFF_DEL=-1
 
 #TOOLS e DATABASES
 REF_FASTA="/home/projects2/LIDO/molPathol/oncoseek/nextseq/hg38/"
-ANNOVAR="$SCRATCH60/tools/annovar/table_annovar.pl"
-ANNOVAR_DB="$SCRATCH60/humandb/"
-GATK="$SCRATCH60/tools/gatk-4.3.0.0/./gatk"
-TARGET="$SCRATCH60/references/xgen-exome-research-panel-v2-targets-hg38.autossome.bed"
+ANNOVAR="$SCRATCH90/tools/annovar/table_annovar.pl"
+ANNOVAR_DB="$SCRATCH90/humandb/"
+GATK="$SCRATCH90/tools/gatk-4.3.0.0/./gatk"
+GATK="$SCRATCH90/tools/gatk-4.6.0.0/./gatk"
+TARGET="$SCRATCH90/references/xgen-exome-research-panel-v2-targets-hg38.autossome.bed"
+dataSourcesFolder="/home/scratch90/vlira_05ago2024/references/funcotator_dataSources.v1.8.hg38.20230908s/"
 
 PON="/home/users/vlira/PanelOfNornal/PON.100COVID.100-eigensamples.hdf5"
 
 mkdir $OUTPUT_DIR
 
-find "$INPUT_DIR" -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam' | grep -Pv "ROP-25-|ROP-26-|ROP-27-|ROP-29-" > $OUTPUT_DIR/samples.list
-head -3 $OUTPUT_DIR/samples.list >  $OUTPUT_DIR/TOY.samples.list
+#find "$INPUT_DIR" -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam' | grep -Pv "ROP-25-|ROP-26-|ROP-27-|ROP-29-" > $OUTPUT_DIR/samples.list
+#head -3 $OUTPUT_DIR/samples.list >  $OUTPUT_DIR/TOY.samples.list
 OUTPUT_LOG="$OUTPUT_DIR.log"
 
 export OUTPUT_DIR
@@ -44,6 +47,7 @@ export ANNOVAR_DB
 export GATK
 export PON
 export TARGET
+export dataSourcesFolder
 
 export MAXmem
 export COPY_RATIO
@@ -271,8 +275,31 @@ step10_FilterCallCopyRatioSegments (){
 }
 export -f step10_FilterCallCopyRatioSegments
 
+ dataSourcesFolder="/home/scratch90/vlira_05ago2024/references/funcotator_dataSources.v1.8.hg38.20230908s/"
+# GATK="/home/scratch90/vlira_05ago2024/tools/gatk-4.6.0.0/./gatk"
+# REF_FASTA="/home/projects2/LIDO/molPathol/oncoseek/nextseq/hg38/"
+# OUTPUT_DIR="/home/scratch90/vlira_05ago2024/Result_Gatk-CNV.2023-11-07/"
+# mkdir "${OUTPUT_DIR}/step10_FuncotateSegments/"
+# NAME="ROP-98-ExC85-xgenV2_S66.dedup.tags.bqsr.bam"
 
+step10_FuncotateSegments (){
+  local SAMPLE=$1
+  local NAME="${SAMPLE##*/}"
 
+  echo "" >> $OUTPUT_LOG
+  echo ">>>>>> Executando step10_FuncotateSegments para amostra: $NAME <<<" >> $OUTPUT_LOG
+  date >> $OUTPUT_LOG
+
+  ${GATK} FuncotateSegments \
+    --data-sources-path ${dataSourcesFolder} \
+    --ref-version hg38 \
+    --output-file-format SEG \
+    -R  ${REF_FASTA}/Homo_sapiens_assembly38.fasta \
+    --segments $OUTPUT_DIR/step8_CallCopyRatioSegments/${NAME}.called.seg \
+    -O ${OUTPUT_DIR}/step10_FuncotateSegments/${NAME}.funcotated.tsv 2> ${OUTPUT_DIR}/step10_FuncotateSegments/${NAME}.log2
+  # --transcript-list tx_list.txt 
+}
+export -f step10_FuncotateSegments
 
 
 echo "                           >>>>>> Starting Pipeline to Run Pipeline_GATK-CNV.sh <<<<<<" >> $OUTPUT_LOG
@@ -291,8 +318,8 @@ mkdir $OUTPUT_DIR/step4_DenoiseReadCounts/
 # xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step4_DenoiseReadCounts  "$@"' 'step4_DenoiseReadCounts'
 
 mkdir $OUTPUT_DIR/step5_PlotDenoisedCopyRatios/
-#xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step5_PlotDenoisedCopyRatios  "$@"' 'step5_PlotDenoisedCopyRatios'
-xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step5_PlotDenoisedCopyRatios_DOCKER  "$@"' 'step5_PlotDenoisedCopyRatios_DOCKER'
+# #xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step5_PlotDenoisedCopyRatios  "$@"' 'step5_PlotDenoisedCopyRatios'
+# xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step5_PlotDenoisedCopyRatios_DOCKER  "$@"' 'step5_PlotDenoisedCopyRatios_DOCKER'
 
 mkdir $OUTPUT_DIR/step6_CollectAllelicCounts/
 # xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step6_CollectAllelicCounts  "$@"' 'step6_CollectAllelicCounts'
@@ -303,15 +330,44 @@ mkdir $OUTPUT_DIR/step7_ModelSegments/
 mkdir $OUTPUT_DIR/step8_CallCopyRatioSegments/
 # xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step8_CallCopyRatioSegments  "$@"' 'step8_CallCopyRatioSegments'
 
+cat  $OUTPUT_DIR/step8_CallCopyRatioSegments/ROP-*.called.igv.seg | grep "Sample"| head -1 > $OUTPUT_DIR/step8_CallCopyRatioSegments/ALL-ROP.called.igv.seg
+cat  $OUTPUT_DIR/step8_CallCopyRatioSegments/ROP-*.called.igv.seg | grep -v "Sample" >> $OUTPUT_DIR/step8_CallCopyRatioSegments/ALL-ROP.called.igv.seg
+
 mkdir $OUTPUT_DIR/step9_PlotModeledSegments/
-# xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step9_PlotModeledSegments  "$@"' 'step9_PlotModeledSegments'
-xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step9_PlotModeledSegments_DOCKER  "$@"' 'step9_PlotModeledSegments_DOCKER'
+# # xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step9_PlotModeledSegments  "$@"' 'step9_PlotModeledSegments'
+# xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step9_PlotModeledSegments_DOCKER  "$@"' 'step9_PlotModeledSegments_DOCKER'
 
 # mkdir $OUTPUT_DIR/step10_FilterCallCopyRatioSegments/
 # xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step10_FilterCallCopyRatioSegments  "$@"' 'step10_FilterCallCopyRatioSegments'
 
 # cat  $OUTPUT_DIR/step10_FilterCallCopyRatioSegments/ROP-*.called.filt.igv.seg | grep "Sample"| head -1 > $OUTPUT_DIR/step10_FilterCallCopyRatioSegments/ALL-ROP.called.filt.igv.seg
 # cat  $OUTPUT_DIR/step10_FilterCallCopyRatioSegments/ROP-*.called.filt.igv.seg | grep -v "Sample" >> $OUTPUT_DIR/step10_FilterCallCopyRatioSegments/ALL-ROP.called.filt.igv.seg
+
+mkdir $OUTPUT_DIR/step10_FuncotateSegments/
+xargs -a $OUTPUT_DIR/samples.list -t -n1 -P${JOBS} bash -c 'step10_FuncotateSegments  "$@"' 'step10_FuncotateSegments'
+
+
+# 6- REUNI OS ARQUIVOS ANOTADOS EM UMA UNICA TABELA
+SAMPLES=$(find $OUTPUT_DIR/step10_FuncotateSegments/ -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam.funcotated.tsv.gene_list.txt')
+echo > $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.gene_list.txt
+echo > $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.funcotated.tsv
+for SAMPLE in $SAMPLES; do
+  NAME="${SAMPLE##*/}"
+  echo ${NAME%.dedup*}
+  cut -f 1,2,3,4,7,10,11,12  $OUTPUT_DIR/step10_FuncotateSegments/${NAME%.dedup*}.dedup.tags.bqsr.bam.funcotated.tsv.gene_list.txt| awk -OFS="\t" -v N=${NAME%.dedup*} '{print N,$_ }' >> $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.gene_list.txt
+  cut -f 3,5,8,9,11,15,17,18,19,20,21  $OUTPUT_DIR/step10_FuncotateSegments/${NAME%.dedup*}.dedup.tags.bqsr.bam.funcotated.tsv| awk -OFS="\t" -v N=${NAME%.dedup*} '{print N,$_ }'  >> $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.funcotated.tsv
+done
+
+# TABELA RESULTADO FINAL 
+sed -i 's/\s/\t/' $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.funcotated.tsv
+sed -i 's/\s/\t/'  $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.gene_list.txt
+
+# cut -f 1,2,3,4,7,10,11,12  $OUTPUT_DIR/step10_FuncotateSegments/ROP-*.dedup.tags.bqsr.bam.funcotated.tsv.gene_list.txt | grep "sample"| head -1 > $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.gene_list.txt
+# cut -f 1,2,3,4,7,10,11,12  $OUTPUT_DIR/step10_FuncotateSegments/ROP-*.dedup.tags.bqsr.bam.funcotated.tsv.gene_list.txt | grep -v "sample" >> $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.gene_list.txt
+
+# cut -f 3,5,7,8,9,11,15,17,18,19,20,21  $OUTPUT_DIR/step10_FuncotateSegments/ROP-*.dedup.tags.bqsr.bam.funcotated.tsv | grep "sample"| head -1 > $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.funcotated.tsv
+# cut -f 3,5,7,8,9,11,15,17,18,19,20,21  $OUTPUT_DIR/step10_FuncotateSegments/ROP-*.dedup.tags.bqsr.bam.funcotated.tsv | grep -v "sample" >> $OUTPUT_DIR/step10_FuncotateSegments/ALL-ROP.funcotated.tsv
+
 
 echo "" >> $OUTPUT_LOG
 echo "                           >>>>>> End Pipeline <<< " >> $OUTPUT_LOG
